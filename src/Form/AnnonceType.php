cb7c149b6d7ad07833316657810952c5aa663955
Nassgr <anas.grina@hotmail.fr>
2018-07-12 11:46:41 +0200

    adminannonce Controller

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +16,12 @@ class AnnonceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('categorie')
-            ->add('type')
-            ->add('title')
-            ->add('description')
-            ->add('price')
-            ->add('city')
+            ->add('categorie', RadioType::class)
+            ->add('type', RadioType::class)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('price', IntegerType::class)
+            ->add('city', TextType::class)
         ;
     }
 
